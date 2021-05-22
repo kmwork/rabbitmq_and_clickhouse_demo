ccff4999466736534336150bf352161924090d9b
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-05-22 12:32:52 +0300

    init demo

--- src/main/resources/ch.create.sql
+++ src/main/resources/ch.create.sql
@@ -1,12 +1,12 @@
 create database itx2_events_demo_kostya;
 
-
 create table itx2_events_demo_kostya.itx2_events_demo
 (
     id    UInt64,
     key   VARCHAR(255),
     value VARCHAR(255)
-) engine = MergeTree ORDER BY id
-SETTINGS index_granularity = 8192;
+) engine = MergeTree ORDER BY (id, key) PRIMARY KEY (id, key)
+      SETTINGS index_granularity = 8192;
+
 
 select timezone(), version()
