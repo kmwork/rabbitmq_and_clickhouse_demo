6fbdc525b499fa742bc952d3e5ed4d3b6d8459e3
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-05-22 11:30:28 +0300

    init demo

--- src/main/java/ru/kac/Main.java
+++ src/main/java/ru/kac/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,9 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import ru.yandex.clickhouse.ClickHouseConnection;
 import ru.yandex.clickhouse.ClickHouseDataSource;
-import ru.yandex.clickhouse.ClickHouseStatement;
 import ru.yandex.clickhouse.settings.ClickHouseProperties;
-import ru.yandex.clickhouse.settings.ClickHouseQueryParam;
 
 @Slf4j
 public class Main {
@@ -38,6 +35,7 @@ public class Main {
         String url = prop.getProperty("ch.url");
         String userName = prop.getProperty("ch.username");
         String password = prop.getProperty("ch.password");
+        String table = prop.getProperty("ch.table");
         ClickHouseProperties properties = new ClickHouseProperties();
         properties.setClientName("Agent-Kostya");
         properties.setUser(userName);
@@ -45,36 +43,42 @@ public class Main {
 
         ClickHouseDataSource chDS = new ClickHouseDataSource(url, properties);
         try (ClickHouseConnection chConn = chDS.getConnection() {
-            PreparedStatement statement = chConn.prepareStatement("INSERT INTO test.batch_insert (s, i) VALUES (?, ?), (?, ?)");
+
+            StringBuilder sqlBuilder = new StringBuilder();
+            sqlBuilder.append("INSERT INTO ").append(table);
+            sqlBuilder.append(".batch_insert ").append("(id, key,value) VALUES ");
+            for (int i = 0; i < result.size(); i++) {
+                if (i > 0)
+                    sqlBuilder.append(", ");
+                sqlBuilder.append("(?, ?,?)");
+            }
+            String sql = sqlBuilder.toString();
+            log.debug("[SQL] sql = " + sql);
+
+            PreparedStatement statement = chConn.prepareStatement(sql);
             for (Map.Entry<String, Object> elem : result.entrySet()) {
                 long id = System.nanoTime();
                 String key = elem.getKey();
-                String value = elem.getValue() elem.getValue().toString();
+                String value = elem.getValue() == null ? null : elem.getValue().toString();
 
                 log.debug("[SQL:INSERT] id = {}, key = {}, value = {}", id, key, value);
                 statement.setLong(1, id);
                 statement.setString(2, key);
                 statement.setString(2, value);
+                statement.execute();
             }
 
-            statement.setLong(1, id);
-            statement.setInt(2, 21);
-            statement.setString(3, "string2");
-            statement.setInt(4, 22);
-            statement.execute();
         } catch (SQLException e) {
             log.error("[SQL:Error]", e);
         }
-        chDS.
 
-                String sql = "select * from mytable";
-        Map<ClickHouseQueryParam, String> additionalDBParams = new HashMap<>();
-// set request options, which will override the default ones in ClickHouseProperties
-        additionalDBParams.put(ClickHouseQueryParam.SESSION_ID, "new-session-id");
-
-        try (ClickHouseConnection conn = dataSource.getConnection();
-             ClickHouseStatement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql, additionalDBParams)) {
-        }
+//        Map<ClickHouseQueryParam, String> additionalDBParams = new HashMap<>();
+//// set request options, which will override the default ones in ClickHouseProperties
+//        additionalDBParams.put(ClickHouseQueryParam.SESSION_ID, "new-session-id");
+//
+//        try (ClickHouseConnection conn = dataSource.getConnection();
+//             ClickHouseStatement stmt = conn.createStatement();
+//             ResultSet rs = stmt.executeQuery(sql, additionalDBParams)) {
+//        }
     }
 }
---
+++ src/main/resources/ch.create.sql
@@ -0,0 +1,11 @@
+create database itx2_events_demo_kostya;
+
+
+create table itx2_events_demo_kostya.itx2_events_demo
+(
+    id    UInt64,
+    key   UInt64,
+    value UInt64
+) engine = MergeTree ORDER BY id
+SETTINGS index_granularity = 8192;
+
